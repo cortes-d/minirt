48e6ad73bdf2e5fed179f03c12fbfac5bd36ea98
9dvst1 <damian.cortes@me.com>
2024-08-01 13:02:48 +0200

    feat: add mat4_submatrix

---
+++ src/libla/mat4_submatrix.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mat4_submatrix.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 11:59:51 by dcortes           #+#    #+#             */
+/*   Updated: 2024/08/01 13:01:51 by dcortes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libla.h"
+
+t_mat3	mat4_submatrix(const t_mat4 m, int row, int col)
+{
+	t_mat3	submatrix;
+	int		i;
+	int		j;
+	int		sub_i;
+	int		sub_j;
+
+	i = 0;
+	sub_i = 0;
+	while (i < 4)
+	{
+		if (i != row)
+		{
+			j = 0;
+			sub_j = 0;
+			while (j < 4)
+			{
+				if (j != col)
+				{
+					submatrix.data[sub_i][sub_j] = m.data[i][j];
+					sub_j++;
+				}
+				j++;
+			}
+			sub_i++;
+		}
+		i++;
+	}
+	return (submatrix);
+}
