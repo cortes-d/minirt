a1b8162357be169c8f4612b0a91f4edb6dc69d81
9dvst1 <damian.cortes@me.com>
2024-08-01 23:38:29 +0200

    fix(minor): fix reference

--- src/libla/matrix/minor/mat3_minor.c
+++ src/libla/matrix/minor/mat3_minor.c
@@ -6,13 +6,16 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 16:04:22 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/01 17:15:08 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/01 23:11:43 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libla.h"
 
-float	mat3_minor(const t_mat3 *m, const row, const col)
+float	mat3_minor(const t_mat3 *m, const int row, int const col)
 {
-	return (mat2_det(mat3_submatrix(m, row, col)));
+	t_mat2	submatrix;
+
+	submatrix = mat3_submatrix(m, row, col);
+	return (mat2_det(&submatrix));
 }
--- src/libla/matrix/minor/mat4_minor.c
+++ src/libla/matrix/minor/mat4_minor.c
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 17:14:44 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/01 17:48:56 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/01 23:12:46 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ float	mat4_minor(const t_mat4 *m, const row, const col)
 {
 	t_mat3	submatrix;
 
-	submatrix = mat4_submatrix(m, &row, &col);
+	submatrix = mat4_submatrix(m, row, col);
 	return (mat3_det(&submatrix));
 }
