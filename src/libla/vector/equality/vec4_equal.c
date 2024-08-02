94b1af733c5865fafb623b2b7de1018f6d0e7403
aiko <aiko@imac-de-lab7.home>
2024-08-02 09:02:44 +0200

    fix(vec4_equal.c): wrong code

--- src/libla/vector/equality/vec4_equal.c
+++ src/libla/vector/equality/vec4_equal.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mat4_equal.c                                       :+:      :+:    :+:   */
+/*   vec4_equal.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 08:36:51 by achappui          #+#    #+#             */
-/*   Updated: 2024/08/02 08:50:54 by achappui         ###   ########.fr       */
+/*   Updated: 2024/08/02 08:59:53 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libla.h"
 
-bool	mat4_equal(t_mat4 *a, t_mat4 *b)
+bool	vec4_equal(t_vec4 *v1, t_vec4 *v2)
 {
 	short	r;
-	short	c;
 
 	r = 0;
 	while (r < 4)
 	{
-		c = 0;
-		while (++c < 4)
-		{
-			if (!equalf(a->data[r][c], b->data[r][c]))
-				return (0);
-			c++;
-		}
+		if (!equalf(v1->data[r], v2->data[r]))
+			return (0);
 		r++;
 	}
 	return (1);
