bcfcefa716f5b9ffec39f652ccfb5f6001fe1c69
dcortes <damian.cortes@me.com>
2024-08-04 11:52:25 +0200

    add: rotation

--- src/libla/matrix/transformation/mat4_rotation_y.c
+++ src/libla/matrix/transformation/mat4_rotation_y.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mat4_rotation_x.c                                  :+:      :+:    :+:   */
+/*   mat4_rotation_y.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damian-cortes <damian-cortes@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 11:40:21 by damian-cort       #+#    #+#             */
-/*   Updated: 2024/08/04 11:45:34 by damian-cort      ###   ########.fr       */
+/*   Updated: 2024/08/04 11:50:07 by damian-cort      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "libla.h"
 
-t_mat4  mat4_rotation_x(float r)
+t_mat4  mat4_rotation_y(float r)
 {
-	t_mat4	rotation_x;
+	t_mat4	rotation_y;
 
-	rotation_x = mat4( \
-		row4(1, 0, 0, 0), \
-		row4(0, cos(r), -sin(r), 0), \
-		row4(0, sin(r), cos(r), 0), \
+	rotation_y = mat4( \
+		row4(cos(r), 0, sin(r), 0), \
+		row4(0, 1, 0, 0), \
+		row4(-sin(r), 0, cos(r), 0), \
 		row4(0, 0, 0, 1));
-	return (rotation_x);
+	return (rotation_y);
 }
