2b0bfab87672e4064e7870353b484a68d582fd5f
Damian Florian Cortes <dcortes@c1r17s3.42lausanne.ch>
2024-08-05 15:50:16 +0200

    fix(position): remove reference

--- src/raytracer/position.c
+++ src/raytracer/position.c
@@ -6,13 +6,13 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:50:04 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/05 14:57:35 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/05 15:49:41 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raytracer.h"
 
-t_vec4	position(const t_ray *ray, float t)
+t_vec4	position(t_ray ray, float t)
 {
-	return (vec4_add(ray->p_origin, vec4_mul(ray->v_direction, t)));
+	return (vec4_add(ray.p_origin, vec4_mul(ray.v_direction, t)));
 }
