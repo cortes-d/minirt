f05ed94194e7d7aebd87ac6a5ece32d7cececd60
Damian Florian Cortes <dcortes@c1r17s3.42lausanne.ch>
2024-08-05 15:31:51 +0200

    style(mat4_rotation): norminette

--- src/libla/matrix/transformation/mat4_rotation_z.c
+++ src/libla/matrix/transformation/mat4_rotation_z.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   mat4_rotation_z.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: damian-cortes <damian-cortes@student.42    +#+  +:+       +#+        */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 11:40:21 by damian-cort       #+#    #+#             */
-/*   Updated: 2024/08/04 11:51:22 by damian-cort      ###   ########.fr       */
+/*   Updated: 2024/08/05 15:30:18 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "libla.h"
 
-t_mat4  mat4_rotation_z(float r)
+t_mat4	mat4_rotation_z(float r)
 {
 	t_mat4	rotation_z;
 
-	rotation_z = mat4( \
+	rotation_z = mat4(\
 		row4(cos(r), -sin(r), 0, 0), \
 		row4(sin(r), cos(r), 0, 0), \
 		row4(0, 0, 1, 0), \
