bca150ea4eee0626c7f2e44856024cd0d61c87fc
aiko <aiko@imac-de-lab7.home>
2024-08-12 14:16:03 +0200

    fix(str_float_check.c): if statement fixed

--- src/parser/convertion_check/str_float_check.c
+++ src/parser/convertion_check/str_float_check.c
@@ -6,7 +6,7 @@
 /*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 13:36:28 by achappui          #+#    #+#             */
-/*   Updated: 2024/08/12 14:05:47 by achappui         ###   ########.fr       */
+/*   Updated: 2024/08/12 14:15:37 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	str_float_check(const char **str)
 	if (!((**str >= '0' && **str <= '9') || **str == '+' || **str == '-'))
 		return (ERROR);
 	(*str)++;
-	if (!(**str >= '0' && **str <= '9') || '.')
+	if (!((**str >= '0' && **str <= '9') || **str == '.'))
 		return (ERROR);
 	while (**str == '0' && *(*str + 1) == '0')
 		(*str)++;
