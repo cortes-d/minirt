4590b75ebedbd0e3135341101709f4844669b2b6
cortes-d <dcortes@c1r16s7.42lausanne.ch>
2024-08-13 16:16:49 +0200

    fix: swap

--- src/util/swap.c
+++ src/util/swap.c
@@ -5,13 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< Updated upstream:src/util/swap.c
-/*   Created: 2024/08/13 14:48:00 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/13 14:50:45 by dcortes          ###   ########.fr       */
-=======
 /*   Created: 2024/08/11 11:40:02 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/12 14:56:05 by dcortes          ###   ########.fr       */
->>>>>>> Stashed changes:src/parser/creation/params_from_tokens.c
+/*   Updated: 2024/08/13 16:16:34 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +14,8 @@ void	swap(float *a, float *b)
 {
 	float	tmp;
 
-<<<<<<< Updated upstream:src/util/swap.c
 	tmp = *b;
 	*b = *a;
 	*a = tmp;
-=======
-	current = tokens->next;
-	i = 1;
-	while (i < count)
-	{
-		params[i] = current->content;
-		current = current->next;
-		i++;
-	}
->>>>>>> Stashed changes:src/parser/creation/params_from_tokens.c
 }
 
