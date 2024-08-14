edd81af8a06727eed87dfb729185a910e798d63d
cortes-d <dcortes@c1r16s7.42lausanne.ch>
2024-08-14 14:08:04 +0200

    chore: reorganization

--- inc/raytracer.h
+++ inc/raytracer.h
@@ -6,33 +6,28 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 22:18:09 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/14 10:27:56 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/14 14:05:22 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYTRACER_H
 # define RAYTRACER_H
-# include "libft.h"
+
+# include "libft.h" // t_list
 # include "libla.h"
+# include "scene.h"
 
 // =============================================================================
 // Section : Constants and Macros
 // =============================================================================
 
-# define CAMERA_HSIZE 500
-# define CAMERA_VSIZE 500
-
 // =============================================================================
 // Section : Type Definitions
 // =============================================================================
 
-// --- Enum ---
-typedef enum e_object_type
-{
-	SPHERE,
-	PLANE,
-	CYLINDER
-}	t_object_type;
+// ·············································································
+// Sub-section : Ray
+// ·············································································
 
 // --- Structure : Ray ---
 typedef struct s_ray
@@ -41,56 +36,9 @@ typedef struct s_ray
 	t_vec4	v_direction;
 }	t_ray;
 
-// --- Structure : Material ---
-typedef struct s_material
-{
-	t_vec3	color;
-	float	ambient;
-	float	diffuse;
-	float	specular;
-	float	shininess;
-}	t_material;
-
-// --- Structure : Sphere ---
-typedef struct s_sphere
-{
-	t_vec4	p_origin;
-	float	radius;
-}	t_sphere;
-
-// --- Structure : Cylinder ---
-typedef struct s_cylinder
-{
-	t_vec4	p_origin;
-	t_vec4	v_axis;
-	float	height;
-	float	diameter;
-	float	minimum;
-	float	maximum;
-}	t_cylinder;
-
-// --- Structure : Plane ---
-typedef struct s_plane
-{
-	t_vec4	p_point;
-	t_vec4	v_normal;
-}	t_plane;
-
-// --- Structure : Object ---
-typedef struct s_object
-{
-	t_object_type	type;
-	t_mat4			transform;
-	t_mat4			transform_inverse;
-	t_material		material;
-	union
-	{
-		t_sphere	sphere;
-		t_cylinder	cylinder;
-		t_plane		plane;
-	}	u_object;
-
-}	t_object;
+// ·············································································
+// Sub-section : Intersection
+// ·············································································
 
 // --- Structure : Intersection ---
 typedef struct s_intersection_pair
@@ -105,24 +53,9 @@ typedef struct s_intersection
 	t_object	object;
 }	t_intersection;
 
-// --- Structure : Camera ---
-typedef struct s_camera
-{
-	t_mat4			transform;
-	unsigned int	hsize;
-	unsigned int	vsize;
-	float			half_width;
-	float			half_height;
-	float			pixel_size;
-}	t_camera;
-
-// === Lighting ===
-// --- Structure : Point light ---
-typedef struct s_light_point
-{
-	t_vec4	p_origin;
-	t_vec3	color;
-}	t_light_point;
+// ·············································································
+// Sub-section : Light
+// ·············································································
 
 // --- Structure : Shading ---
 typedef struct s_shading
@@ -130,15 +63,6 @@ typedef struct s_shading
 	t_vec4	eyev;
 	t_vec4	normalv;
 }	t_shading;
-
-// --- Structure : World ---
-typedef struct s_world
-{
-	unsigned int	lights_count;
-	t_light_point	*lights;
-	unsigned int	objects_count;
-	t_object		*objects;
-}	t_world;
 
 // =============================================================================
 // Section : Functions
@@ -167,35 +91,5 @@ t_intersection		*hit(t_list *intersections);
 t_ray				ray_transform(t_ray ray, t_mat4 transformation);
 void				set_transform(t_object *object, t_mat4 transformation);
 void				add_transform(t_object *object, t_mat4 transformation);
-
-// --- Color ---
-t_vec3				color_rgb_f(float r, float g, float b);
-t_vec3				color_rgb_i(int r, int g, int b);
-
-// --- Material ---
-t_material			material(void);
-t_material			material_from_color(float r, float g, float b);
-void				set_object_color(t_object *object, t_vec3 color);
-
-// --- Light ---
-t_light_point		light_point(t_vec4 position, t_vec3 color);
-t_vec3				lighting(t_material material, t_light_point light, \
-						t_vec4 intersection_point, t_shading shading);
-t_vec4				normal_at(t_object object, t_vec4 world_point);
-t_vec4				reflect(t_vec4 in, t_vec4 normal);
-
-// --- Geometry ---
-t_vec4				point(float x, float y, float z);
-t_vec4				vector(float x, float y, float z);
-t_object			sphere(void);
-t_object			sphere_from_params(t_vec4 p_origin, float radius);
-t_object			cylinder(void);
-t_object			cylinder_from_params(t_vec4 p_origin, \
-						t_vec4 v_axis, float diameter, float height);
-t_object			plane(void);
-t_object			plane_from_params(t_vec4 p_point, t_vec4 v_normal);
-
-// --- World ---
-t_world				world(void);
 
 #endif
