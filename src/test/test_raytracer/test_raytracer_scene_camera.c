275e53122527f89ce06a4fe1bac4d35fc6b4b4b7
dcortes <dcortes@student.42lausanne.ch>
2024-08-21 11:07:34 +0200

    test: fix test

--- src/test/test_raytracer/test_raytracer_scene_camera.c
+++ src/test/test_raytracer/test_raytracer_scene_camera.c
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 16:18:56 by achappui          #+#    #+#             */
-/*   Updated: 2024/08/20 15:57:46 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/21 11:00:33 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,23 @@ int	test_raytracer_scene_camera(void)
 {
 	void				*mlx_ptr;
 	void				*mlx_win;
-	t_image				img;
+	t_image				*img;
     t_scene             scene;
 
 	// mlx
 	mlx_ptr = mlx_init();
-	mlx_win = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "TEST_PHONG");
-	img.img = mlx_new_image(mlx_ptr, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx_win = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "TEST_CAMERA");
+	img = malloc(sizeof(t_image));
+	img->img = mlx_new_image(mlx_ptr, WIDTH, HEIGHT);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
 
 
 	// scene
     scene = scene_default();
-
+	scene.camera = malloc(sizeof(t_camera));
 	*scene.camera = camera_default();
-	render(scene, &img);
-	mlx_put_image_to_window(mlx_ptr, mlx_win, img.img, 0, 0);
+	render(scene, img);
+	mlx_put_image_to_window(mlx_ptr, mlx_win, img->img, 0, 0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
