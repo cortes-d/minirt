9bbbffd40eda53a9d841f09c952a47dbe3748be2
aiko <aiko@imac-de-lab7.home>
2024-08-26 17:12:16 +0200

    test: fixed test_parser

--- src/test/test_parser/test_parser.c
+++ src/test/test_parser/test_parser.c
@@ -6,7 +6,7 @@
 /*   By: achappui <achappui@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:56:59 by achappui          #+#    #+#             */
-/*   Updated: 2024/08/21 12:59:20 by achappui         ###   ########.fr       */
+/*   Updated: 2024/08/26 17:11:55 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int	main(void)
 {
-	t_scene	scene;
+	t_scene_static	*scene_static;
 
-	scene = scene_empty();
+	scene_static_init("src/test/test_parser/test_scene.rt");
+	scene_static = *scene_static_get();
 	printf("A 0.200 255,255,255\nC -50,0,20 0,0,1 70.000\nL -40,0,30 0.700 255,255,255\npl 0,0,0 0,1.0,0 255,0,225\nsp 0,0,20 20.000 255,0,0\ncy 50,0,20.6 0,0,1.0 14.200 21.4200 10,0,255\n");
 	printf("\n");
-	parse_scene("src/test/test_parser/test_scene.rt", &scene);
-	test_print_scene(&scene);
+	parse_scene();
+	test_print_scene(&scene_static->scene);
 	return (0);
 }
 
