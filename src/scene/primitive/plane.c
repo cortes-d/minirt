7d1eb8568f5c3540d7525979c2ce47c627b99bf3
aiko <aiko@imac-de-lab7.home>
2024-08-26 15:00:34 +0200

    revenir au dernier commit

--- src/scene/primitive/plane.c
+++ src/scene/primitive/plane.c
@@ -6,23 +6,50 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 13:03:06 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/26 14:38:41 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/08/26 09:23:55 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raytracer.h"
 
-t_object plane(t_vec4 p_point, t_vec4 v_normal)
-{
-	t_object object;
-	float theta_x;
-	float theta_z;
+t_object plane(t_vec4 p_point, t_vec4 v_normal) {
+    t_object object;
+    float theta_x;
+    float theta_z;
+    t_mat4 rotation_x;
+    t_mat4 rotation_z;
+    t_mat4 rotation;
+    t_mat4 translation;
 
-	object = plane_default();
-	theta_z = atan2f(v_normal.data[X], v_normal.data[Y]);
-	theta_x = atan2f(v_normal.data[Z], sqrtf(v_normal.data[X] * v_normal.data[X] + v_normal.data[Y] * v_normal.data[Y]));
-	add_transform(&object, mat4_rotation_z(theta_z));
-	add_transform(&object, mat4_rotation_x(theta_x));
-	add_transform(&object, mat4_translation(p_point.data[X], p_point.data[Y], p_point.data[Z]));
-	return (object);
+    object = plane_default();
+
+    // Modify the plane's point and normal
+    object.u_object.plane.p_point = p_point;
+    object.u_object.plane.v_normal = v_normal;
+
+    // Calculate the rotation angles
+    theta_z = atan2f(v_normal.data[X], v_normal.data[Y]);
+    theta_x = atan2f(v_normal.data[Z], sqrtf(v_normal.data[X] * v_normal.data[X] + v_normal.data[Y] * v_normal.data[Y]));
+
+    // Create rotation matrices
+    rotation_z = mat4_rotation_z(theta_z);
+    rotation_x = mat4_rotation_x(theta_x);
+
+    // Combine the rotations: rotation = rotation_x * rotation_z
+    rotation = mat4_mat4_mul(rotation_x, rotation_z);
+
+	vec4_print(p_point);
+
+    // Create translation matrix
+    translation = mat4_translation(p_point.data[X], p_point.data[Y], p_point.data[Z]);
+	mat4_print(translation);
+
+    // Combine the rotation and translation: transform = translation * rotation
+    object.transform = mat4_mat4_mul(translation, rotation);
+	mat4_print(object.transform);
+
+    // Calculate the inverse of the transform matrix
+    object.transform_inverse = mat4_inv(object.transform);
+
+    return object;
 }
