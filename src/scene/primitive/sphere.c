b92680e5db6e97a15f1dbee5924bd6820b049da8
9dvst1 <damian.cortes@me.com>
2024-08-27 15:26:24 +0200

    refactor: sphere

--- src/scene/primitive/sphere.c
+++ src/scene/primitive/sphere.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achappui <achappui@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/12 10:49:17 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/22 13:16:42 by achappui         ###   ########.fr       */
+/*   Created: 2024/08/27 15:25:51 by dcortes           #+#    #+#             */
+/*   Updated: 2024/08/27 15:25:51 by dcortes          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ t_object	sphere(t_vec4 p_origin, float radius)
 	t_object	s;
 
 	s = sphere_default();
-	add_transform(&s, mat4_translation(p_origin.data[X], p_origin.data[Y], p_origin.data[Z]));
+	add_transform(&s, \
+		mat4_translation(p_origin.data[X], p_origin.data[Y], p_origin.data[Z]));
 	add_transform(&s, mat4_scaling(radius, radius, radius));
 	s.transform_inverse = mat4_inv(s.transform);
 	s.u_object.sphere.p_origin = p_origin;
