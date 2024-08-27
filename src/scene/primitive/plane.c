55b8bd1983fb50b216993c57af0eb94bd8e66b5a
9dvst1 <damian.cortes@me.com>
2024-08-27 15:25:41 +0200

    refactor: plane

--- src/scene/primitive/plane.c
+++ src/scene/primitive/plane.c
@@ -5,26 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/27 10:16:29 by dcortes           #+#    #+#             */
-/*   Updated: 2024/08/27 10:22:20 by dcortes          ###   ########.ch       */
+/*   Created: 2024/08/27 15:24:37 by dcortes           #+#    #+#             */
+/*   Updated: 2024/08/27 15:25:15 by dcortes          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raytracer.h"
 
-t_object plane(t_vec4 p_point, t_vec4 v_normal)
+t_object	plane(t_vec4 p_point, t_vec4 v_normal)
 {
-	t_object object;
-	float theta_x;
-	float theta_z;
+	t_object	object;
+	float		theta_x;
+	float		theta_z;
 
 	object = plane_default();
-	//object.u_object.plane.v_normal = v_normal;
-	//object.u_object.plane.p_point = p_point;
 	theta_z = atan2f(v_normal.data[X], v_normal.data[Y]);
-	theta_x = atan2f(v_normal.data[Z], sqrtf(v_normal.data[X] * v_normal.data[X] + v_normal.data[Y] * v_normal.data[Y]));
+	theta_x = atan2f(v_normal.data[Z], \
+		sqrtf(v_normal.data[X] * v_normal.data[X] + \
+		v_normal.data[Y] * v_normal.data[Y]));
 	add_transform(&object, mat4_rotation_z(theta_z));
 	add_transform(&object, mat4_rotation_x(theta_x));
-	add_transform(&object, mat4_translation(p_point.data[X], p_point.data[Y], p_point.data[Z]));
+	add_transform(&object, \
+		mat4_translation(p_point.data[X], p_point.data[Y], p_point.data[Z]));
 	return (object);
 }
