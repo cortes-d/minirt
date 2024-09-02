cdf4da0f6764acb880851e5cfed36dec4836be7d
dcortes <dcortes@student.42lausanne.ch>
2024-09-02 11:01:01 +0200

    style: norminette

--- lib/libla/vector/clamping/vec3_clamp.c
+++ lib/libla/vector/clamping/vec3_clamp.c
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 10:43:53 by dcortes           #+#    #+#             */
-/*   Updated: 2024/09/02 10:47:28 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/09/02 11:00:31 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_vec3	vec3_clamp(t_vec3 v, float max_value)
 {
-	t_vec3 res;
+	t_vec3	res;
 
 	res.data[X] = clamp(v.data[X], max_value);
 	res.data[Y] = clamp(v.data[Y], max_value);
