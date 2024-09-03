f4aa861b5afb80fa9024f5a65d123762b9384c24
dcortes <dcortes@student.42lausanne.ch>
2024-09-03 11:04:12 +0200

    feat: add scenes

--- run_test_scenes.py
+++ run_test_scenes.py
@@ -1,31 +1,27 @@
 import os
 import subprocess
 
-def run_scenes_concurrently(n):
+def run_scenes_concurrently():
     # Directory containing the scene files
     scene_dir = "test_scenes"
     
     processes = []
 
-    # Loop through each scene file from 1.rt to n.rt
-    for i in range(1, n + 1):
-        scene_file = f"{i}.rt"
+    # Get all files in the scene directory
+    scene_files = [f for f in os.listdir(scene_dir) if os.path.isfile(os.path.join(scene_dir, f))]
+
+    # Loop through each scene file
+    for scene_file in scene_files:
         scene_path = os.path.join(scene_dir, scene_file)
         
-        # Check if the scene file exists
-        if os.path.isfile(scene_path):
-            print(f"Launching scene: {scene_file}")
-            # Launch the ./minirt program with the scene file as argument
-            process = subprocess.Popen(["./minirt", scene_path])
-            processes.append(process)
-        else:
-            print(f"Scene file {scene_file} not found in {scene_dir}")
+        print(f"Launching scene: {scene_file}")
+        # Launch the ./minirt program with the scene file as argument
+        process = subprocess.Popen(["./minirt", scene_path])
+        processes.append(process)
 
     # Wait for all processes to complete
     for process in processes:
         process.wait()
 
 if __name__ == "__main__":
-    # Set the number of scenes
-    number_of_scenes = 9  # Change this number to the total number of scenes
-    run_scenes_concurrently(number_of_scenes)
+    run_scenes_concurrently()
